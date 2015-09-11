a671b8b6127109eac9d409b644c23269ed19202e
Rubén Díaz Martínez <rubendm92@gmail.com>
2015-09-11 18:38:47 +0100

    Refactor

--- src/game.js
+++ src/game.js
@@ -2,16 +2,16 @@ exports = typeof window !== "undefined" && window !== null ? window : global;
 require('./QuestionFactory.js');
 
 exports.Game = function() {
-  var players          = [];
-  var places           = new Array(6);
-  var purses           = new Array(6);
-  var inPenaltyBox     = new Array(6);
+  var players = [];
+  var places = new Array(6);
+  var purses = new Array(6);
+  var inPenaltyBox = new Array(6);
   var questions = questionFactory();
 
-  var currentPlayer    = 0;
+  var currentPlayer = 0;
   var isGettingOutOfPenaltyBox = false;
 
-  var didPlayerWin = function(){
+  var didPlayerWin = function() {
     return !(purses[currentPlayer] == 6)
   };
 
@@ -19,11 +19,15 @@ exports.Game = function() {
     return places[currentPlayer];
   }
 
-  this.isPlayable = function(howManyPlayers){
+  var nextPlayer = function() {
+    currentPlayer = (currentPlayer + 1) % players.length;
+  }
+
+  this.isPlayable = function(howManyPlayers) {
     return howManyPlayers >= 2;
   };
 
-  this.add = function(playerName){
+  this.add = function(playerName) {
     players.push(playerName);
     places[this.howManyPlayers() - 1] = 0;
     purses[this.howManyPlayers() - 1] = 0;
@@ -35,25 +39,25 @@ exports.Game = function() {
     return true;
   };
 
-  this.howManyPlayers = function(){
+  this.howManyPlayers = function() {
     return players.length;
   };
 
-  var askQuestion = function(){
+  var askQuestion = function() {
     console.log(questions.forPlace(currentPlayerPosition()));
   };
 
-  this.roll = function(roll){
+  this.roll = function(roll) {
     console.log(players[currentPlayer] + " is the current player");
     console.log("They have rolled a " + roll);
 
-    if(inPenaltyBox[currentPlayer]){
-      if(roll % 2 != 0){
+    if(inPenaltyBox[currentPlayer]) {
+      if(roll % 2 != 0) {
         isGettingOutOfPenaltyBox = true;
 
         console.log(players[currentPlayer] + " is getting out of the penalty box");
         places[currentPlayer] = places[currentPlayer] + roll;
-        if(places[currentPlayer] > 11){
+        if(places[currentPlayer] > 11) {
           places[currentPlayer] = places[currentPlayer] - 12;
         }
 
@@ -67,7 +71,7 @@ exports.Game = function() {
     }else{
 
       places[currentPlayer] = places[currentPlayer] + roll;
-      if(places[currentPlayer] > 11){
+      if(places[currentPlayer] > 11) {
         places[currentPlayer] = places[currentPlayer] - 12;
       }
 
@@ -77,24 +81,19 @@ exports.Game = function() {
     }
   };
 
-  this.wasCorrectlyAnswered = function(){
-    if(inPenaltyBox[currentPlayer]){
-      if(isGettingOutOfPenaltyBox){
+  this.wasCorrectlyAnswered = function() {
+    if(inPenaltyBox[currentPlayer]) {
+      if(isGettingOutOfPenaltyBox) {
         console.log('Answer was correct!!!!');
         purses[currentPlayer] += 1;
         console.log(players[currentPlayer] + " now has " +
                     purses[currentPlayer]  + " Gold Coins.");
 
         var winner = didPlayerWin();
-        currentPlayer += 1;
-        if(currentPlayer == players.length)
-          currentPlayer = 0;
-
+        nextPlayer();
         return winner;
       }else{
-        currentPlayer += 1;
-        if(currentPlayer == players.length)
-          currentPlayer = 0;
+        nextPlayer();
         return true;
       }
 
@@ -109,23 +108,16 @@ exports.Game = function() {
                   purses[currentPlayer]  + " Gold Coins.");
 
       var winner = didPlayerWin();
-
-      currentPlayer += 1;
-      if(currentPlayer == players.length)
-        currentPlayer = 0;
-
+      nextPlayer();
       return winner;
     }
   };
 
-  this.wrongAnswer = function(){
+  this.wrongAnswer = function() {
 		console.log('Question was incorrectly answered');
 		console.log(players[currentPlayer] + " was sent to the penalty box");
 		inPenaltyBox[currentPlayer] = true;
-
-    currentPlayer += 1;
-    if(currentPlayer == players.length)
-      currentPlayer = 0;
+    nextPlayer();
 		return true;
   };
 };
@@ -142,7 +134,7 @@ do{
 
   game.roll(Math.floor(Math.random()*6) + 1);
 
-  if(Math.floor(Math.random()*10) == 7){
+  if(Math.floor(Math.random()*10) == 7) {
     notAWinner = game.wrongAnswer();
   }else{
     notAWinner = game.wasCorrectlyAnswered();
